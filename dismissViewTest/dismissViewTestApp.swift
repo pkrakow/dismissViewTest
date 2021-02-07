7648f29e6c25ddc2820c4b55c723287be4f70813
Paul Krakow <pkrakow@gmail.com>
2021-02-07 11:03:34 -0800

    Initial Commit

---
+++ dismissViewTest/dismissViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dismissViewTestApp.swift
+//  dismissViewTest
+//
+//  Created by Paul on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct dismissViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
